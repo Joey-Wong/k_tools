3e1b44979a532b62a3fdf7ced2c9d0a5e2cff16d
Your Name <511212586@qq.com>
2025-12-21 03:35:51 +0800

    add-getfiles

--- src/views/GetDirFiles/index.vue
+++ src/views/GetDirFiles/index.vue
@@ -9,13 +9,6 @@
         type="primary"
         :text="sourceDir || '选择操作文件夹'"
       />
-      <BtnLine
-        title="保存结果:"
-        :disabled="progressing"
-        @onClick="choseDir('targetDir')"
-        type="primary"
-        :text="saveDir || '选择保存路径'"
-      />
       <LineWrap title="深度扫描:">
         <n-space>
           <n-radio :checked="isDeep" @change="setIsDeep(true)">是</n-radio>
@@ -23,22 +16,33 @@
         </n-space>
         <p style="color: #999">深度扫描会扫描文件夹中的子文件夹</p>
       </LineWrap>
+      <LineWrap title="保留父目录:">
+        <n-space>
+          <n-radio :checked="isKeepFDir" @change="setIsKeepFDir(true)">是</n-radio>
+          <n-radio :checked="!isKeepFDir" @change="setIsKeepFDir(false)">否</n-radio>
+        </n-space>
+        <p style="color: #999">保留父目录会在保存路径中保留操作文件夹的目录结构</p>
+      </LineWrap>
+      <BtnLine
+        title="保存到:"
+        :disabled="progressing"
+        @onClick="choseDir('targetDir')"
+        type="primary"
+        :text="saveDir || '选择保存位置'"
+      />
       <div style="margin-top: 20px">
-        <n-button :disabled="progressing" @click="start()" type="primary">开始获取</n-button>
+        <n-button :disabled="progressing" @click="start()" type="primary">保存</n-button>
       </div>
     </div>
-    <Log :show="progressing || Done" ref="refIDGetDirFiles" />
   </div>
 </template>
 
 <script>
 import { NButton, useMessage, NSpace, NRadio } from "naive-ui";
-import Log from "@/components/Log";
 import BtnLine from "@/components/BtnLine";
 import LineWrap from "@/components/LineWrap";
 import common from "@/mixins/common";
 
-let GetDirFilesSelf = null;
 export default {
   name: "GetDirFiles",
   mixins: [common],
@@ -48,7 +52,6 @@ export default {
     NRadio,
     BtnLine,
     LineWrap,
-    Log,
   },
   setup() {
     window.$message = useMessage();
@@ -56,78 +59,17 @@ export default {
   data() {
     return {
       isDeep: false,
+      isKeepFDir: true,
     };
   },
-  mounted() {
-    GetDirFilesSelf = this;
-    ipcRenderer.on("GetDirFiles", this.logCB);
-  },
-  beforeUnmount() {
-    ipcRenderer.removeAllListeners("GetDirFiles");
-  },
   methods: {
-    logCB(event, res) {
-      const { Code, Msg, Done, Log } = JSON.parse(res);
-      GetDirFilesSelf.$refs.refIDGetDirFiles.addLog({
-        type: Code !== 0 ? "error" : "",
-        log: Code !== 0 ? Msg : Log,
-      });
-      if (Done) {
-        GetDirFilesSelf.Done = true;
-        GetDirFilesSelf.progressing = false;
-      }
-    },
     setIsDeep(v) {
       this.isDeep = v;
     },
-    test() {
-      if (!this.testFileName) {
-        window.$message.warning("请输入测试文件名");
-        return false;
-      }
-      if (this.isUseReg) {
-        if (!this.regFind) {
-          window.$message.warning("请输入查找正则");
-          return false;
-        }
-        if (!this.regReplace) {
-          window.$message.warning("请输入替换内容");
-          return false;
-        }
-        const reg = new RegExp(this.regFind);
-        this.testFileNameRes = this.testFileName.replace(reg, this.regReplace);
-        return true;
-      }
-      if (this.rules[Number(this.selectedRule)].inputDes && !this.addContent) {
-        window.$message.warning(this.rules[Number(this.selectedRule)].inputDes);
-        return false;
-      }
-      if (this.selectedRule === "0") {
-        this.testFileNameRes = `${this.addContent}${this.testFileName}`;
-        return true;
-      }
-      if (this.selectedRule === "1") {
-        const pointIndex = this.testFileName.lastIndexOf(".");
-        if (pointIndex < 0) {
-          this.testFileNameRes = `${this.testFileName}${this.addContent}`;
-          return true;
-        }
-        const fileName = this.testFileName.substring(0, pointIndex);
-        const ext = this.testFileName.substring(pointIndex + 1);
-        this.testFileNameRes = `${fileName}${this.addContent}.${ext}`;
-        return true;
-      }
-      if (this.selectedRule === "2") {
-        this.testFileNameRes = `${this.testFileName}${this.addContent}`;
-        return true;
-      }
-      if (this.selectedRule === "3") {
-        this.testFileNameRes = `1${this.testFileName}`;
-        return true;
-      }
+    setIsKeepFDir(v) {
+      this.isKeepFDir = v;
     },
     async start() {
-      this.$refs.refIDGetDirFiles.clearLog();
       this.Done = false; // 是否执行完成
       this.progressing = false; // 在执行中
       if (!this.sourceDir) {
@@ -139,19 +81,20 @@ export default {
         window.$message.error(err);
         return false;
       }
-      console.log(files);
-      files.forEach((v) => {
-        GetDirFilesSelf.$refs.refIDGetDirFiles.addLog({
-          type: "",
-          log: v,
+      if (!this.isKeepFDir) {
+        const pos = this.targetDir.length + 1;
+        files.forEach((v, index) => {
+          files[index] = v.slice(pos + 1);
         });
-      });
+      }
+      const filePath = `${this.targetDir}/文件列表_${dayjs().format("YYYYMMDD")}.txt`;
       try {
-        ipcRenderer.invoke("WriteFile", { path: `${this.targetDir}/文件列表.txt`, data: files.join(`\r\n`) });
+        ipcRenderer.invoke("WriteFile", { path: filePath, data: files.join(`\r\n`) });
       } catch (e) {
         window.$message.error(e);
+        return false;
       }
-      window.$message.success(`扫描完成,结果已保存至[${this.targetDir}/文件列表.txt]`);
+      window.$message.success(`文件已保存至[${filePath}]`);
       this.Done = true; // 是否执行完成
       this.progressing = false; // 在执行中
     },
